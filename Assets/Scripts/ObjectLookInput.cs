5533b37c7878a639392c8fae4701968edbc1a1f2
Rachel Friedman <rlfriedm@hamilton.edu>
2015-01-26 18:26:55 -0500

    fixed merging issues

--- Assets/Scripts/ObjectLookInput.cs
+++ Assets/Scripts/ObjectLookInput.cs
@@ -1,21 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ObjectLookInput : MonoBehaviour {
-	public Camera centerCamera;
+	public GameObject centerCamera;
+	public Text label;
+	public GameObject playerController;
+	public bool labelsOn = true;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		RaycastHit hit;
-
-		if (Physics.Raycast (centerCamera.transform.position, Vector3.forward, out hit)) {
-			print(hit.transform.name);
+		if (labelsOn) {
+			RaycastHit hit;
+			if (Physics.Raycast (centerCamera.transform.position, centerCamera.transform.forward, out hit)) {
+				print(hit.transform.name);
+				if (hit.transform.tag != "Scene") {
+					label.text = hit.transform.name;
+					label.color = new Color(1, 1,1, 1);
+					float labelScaleX;
+					if (playerController.transform.position.z > hit.transform.position.z) {
+						labelScaleX = -.05f;
+					}
+					else {
+						labelScaleX = .05f;
+					}
+					label.transform.localScale = new Vector3(labelScaleX, .05f, 1f);
+					label.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y + 1, hit.transform.position.z);
+				}
+			}
+			
+			else {
+				label.text = "";
+			}
 		}
-
 	}
 }
+
