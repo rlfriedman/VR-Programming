4f70cc3a171ee671a578f5d40db0284ca1cac1ce
Rachel Friedman <rlfriedm@hamilton.edu>
2015-01-29 03:56:42 -0500

    slightly modified python code for testing

--- Assets/PythonScripts/cubeCreate.py
+++ Assets/PythonScripts/cubeCreate.py
@@ -13,15 +13,22 @@ class Cube():
 	def __init__(self, x, y, z, color):
 		self._x = x
 		self._y = y
-		self._z = z
+		self.z = z
 		self._color = color
 		self._object = unity.GameObject.CreatePrimitive(unity.PrimitiveType.Cube)
 		self._object.transform.position = unity.Vector3(x, y, z)
 		self._object.renderer.material.color = self._color
 
-
+	def moveForward(self, z):
+		self._object.transform.position = unity.Vector3(self._object.transform.position.x, self._object.transform.position.y, self._object.transform.position.z + z)
+		self.z = self._object.transform.position.z
+		
 	def changeColor(self, newColor):
 		self._object.renderer.material.color = newColor
+
+	def getColor(self):
+		return self._object.renderer.material.color
+
 
 class Sphere():
 	def __init__(self, x, y, z, color):
