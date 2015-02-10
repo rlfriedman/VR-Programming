071de4b7365dce92215fe27c9e401d74933d97de
Rachel Friedman <rlfriedm@hamilton.edu>
2015-02-10 02:18:09 -0500

    Preliminary look to see instance var values
    and some other objectLookInput cleanup

--- Assets/Scripts/ObjectLookInput.cs
+++ Assets/Scripts/ObjectLookInput.cs
@@ -11,50 +11,95 @@ using Microsoft.Scripting.Hosting;
 public class ObjectLookInput : MonoBehaviour {
 	public GameObject centerCamera;
 	public Text label;
+	public InputField attributeField;
 	public GameObject playerController;
 	public bool labelsOn = true;
-	// Use this for initialization
+	IEnumerable objects;
+	ObjectOperations operations;
+	
 	void Start () {
-		
+		objects = PythonInterpreter.scope.GetItems();
+		operations = PythonInterpreter.engine.Operations;
 	}
 
-	string getVarName(GameObject selectedObj) {
-		IEnumerable objects = PythonInterpreter.scope.GetItems();
+	ArrayList getInstanceVars(object instance) {
+		ArrayList instanceVars = new ArrayList ();
+		foreach (var name in operations.GetMemberNames(instance)) {
+			object member = operations.GetMember(instance, name);
 
+			if (!operations.IsCallable(member) && !(name == "__doc__" || name == "__module__" || name == "_object")) { // found an instance variable!
+				instanceVars.Add(name);
+			}
+		}
+		return instanceVars;
+	}
+
+	string getVarName(object instance) { // gets the first variable name the object is stored under
 		foreach (KeyValuePair<string, object> obj in objects) {
-			if (obj.Key != "__doc__")  {
-				if (obj.Value.GetType() == typeof(IronPython.Runtime.Types.OldInstance)) {
-					object instance = obj.Value;
-					//print(PythonInterpreter.engine.Operations.GetCallSignatures(instance));
-					//print(PythonInterpreter.engine.Operations.GetMemberNames(instance));
-					object method = PythonInterpreter.engine.Operations.GetMember(instance, "getObject");
-					GameObject instanceObj = (GameObject) PythonInterpreter.engine.Operations.Invoke(method);
-					if (instanceObj == selectedObj) {
-						return obj.Key;
-					}
+			if (obj.Key != "__doc__") {
+				if (obj.Value == instance) {
+					return obj.Key;
 				}
 			}
 		}
 		return "";
 	}
 
+	object getInstance(GameObject selectedObj) { // returns the IronPython OldInstance of the class we are currently looking at
+		foreach (KeyValuePair<string, object> obj in objects) {
+			if (obj.Key != "__doc__")  {
+				if (obj.Value.GetType() == typeof(IronPython.Runtime.Types.OldInstance)) {
+					object instance = obj.Value;
+					
+					object method = PythonInterpreter.engine.Operations.GetMember(instance, "getObject");
+					
+					GameObject instanceObj = (GameObject) PythonInterpreter.engine.Operations.Invoke(method);
+					if (instanceObj == selectedObj) {
+						return instance;
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	void displayInstanceVars(ArrayList instanceVars, object instance) {
+		string display = "";
+
+		for (int i = 0; i < instanceVars.Count; i++) {
+			string name = (string)instanceVars[i];
+			string value = operations.GetMember(instance, name).ToString();
+			display += instanceVars[i] + "=" + value + "\n";
+		}
+
+		attributeField.text = display;
+	}
 
 	// Update is called once per frame
 	void Update () {
+
+		objects = PythonInterpreter.scope.GetItems(); // get current objects
+
 		if (labelsOn) {
 			RaycastHit hit;
 			if (Physics.Raycast (centerCamera.transform.position, centerCamera.transform.forward, out hit)) {
 				if (hit.transform.tag != "Scene" && hit.transform.tag != "Player") {
-
+					attributeField.gameObject.SetActive(true);
+					object instance;
 					if (hit.transform.parent != null) { // if object a part of a larger one, display its name
+						instance = getInstance(hit.transform.parent.gameObject);
 						label.text = hit.transform.parent.name;
-						label.text = getVarName(hit.transform.parent.gameObject);
 					}
 					else {
+						instance = getInstance(hit.transform.gameObject);
 						label.text = hit.transform.name;
-
-						label.text = getVarName(hit.transform.gameObject);
 					}
+
+					label.text = getVarName(instance);  // set label to var name
+
+					ArrayList instanceVars = getInstanceVars(instance); // all instance variable names
+
+					displayInstanceVars(instanceVars, instance);
 
 					label.color = new Color(1, 1,1, 1);
 					float labelScaleX;
@@ -66,11 +111,14 @@ public class ObjectLookInput : MonoBehaviour {
 					}
 					label.transform.localScale = new Vector3(labelScaleX, .05f, 1f);
 					label.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y + 1, hit.transform.position.z);
+					attributeField.transform.localScale = new Vector3(.1f, .05f, 1f);
+					attributeField.transform.position = new Vector3(hit.transform.position.x + 2, hit.transform.position.y, hit.transform.position.z);
 				}
 			}
 			
 			else {
 				label.text = "";
+				attributeField.gameObject.SetActive(false);
 			}
 		}
 	}
